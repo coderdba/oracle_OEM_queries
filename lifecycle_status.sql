f244b2d1a2333d608e8127f9d54162b655b4c0d7
gowrishdba <gowrishdba@yahoo.com>
2015-08-18 17:26:00 +0530

    Update lifecycle_status.sql

--- lifecycle_status.sql
+++ lifecycle_status.sql
@@ -16,10 +16,27 @@ select t.target_name, t.target_type,  d.property_name,
 --           WORKAROUND - remove 'creation date' from the select list
 select distinct a.database_name, a.creation_date, d.property_display_name, p.property_value
 from  mgmt$db_dbninstanceinfo a,  mgmt$target t, mgmt$target_properties p, mgmt$all_target_prop_defs d
-where a.target_guid = t.target_guid and a.database_name= 'RP16DB2'
+where a.target_guid = t.target_guid and a.database_name= '< a db name >'
        and t.target_guid=p.target_guid
        and p.property_name=d.property_name
        and p.property_value is not null
        and d.property_display_name = 'LifeCycle Status';
+       
+
+-- Lifecyclewise DB list
+column prod_non_prod format a20
+column database_name format a20
+
+select distinct p.property_value prod_non_prod, a.database_name
+from  mgmt$db_dbninstanceinfo a,  mgmt$target t, mgmt$target_properties p, mgmt$all_target_prop_defs d
+where a.target_guid = t.target_guid
+       --and a.database_name= '< a db name >'
+       and t.target_guid=p.target_guid
+       and p.property_name=d.property_name
+       --and p.property_value = 'Production'
+       and d.property_display_name = 'LifeCycle Status'
+order by 1,2
+;
+
        
 
