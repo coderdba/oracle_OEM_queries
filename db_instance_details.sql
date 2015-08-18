cb7ca72fc7c9ed150b973ac7a58609c01eb4879c
gowrishdba <gowrishdba@yahoo.com>
2015-08-18 17:08:29 +0530

    Update db_instance_details.sql

--- db_instance_details.sql
+++ db_instance_details.sql
@@ -1,5 +1,10 @@
 --https://oramanageability.wordpress.com/2013/10/07/leverage-sysman-schema-database-inventory-and-target-lifecycles/
 
 select distinct database_name, trim(host_name), instance_name, creation_date
-from mgmt$db_dbninstanceinfo where DATABASE_NAME like '<a non-rac db name>%' or DATABASE_NAME like '<<a rac db name>>%'
+from mgmt$db_dbninstanceinfo where DATABASE_NAME = 'a db name'
 order by 1,3;
+
+select a.TARGET_NAME, a.database_name, b.target_name, b.target_type 
+from  mgmt$db_dbninstanceinfo a,  mgmt$target b
+where a.target_guid = b.target_guid and a.database_name= 'a db name';
+
